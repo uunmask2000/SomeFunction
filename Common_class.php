3b482a9a47ba3ddc58d135cd43e1175eccf0c51c
Jim <uunmask2000@gmail.com>
2018-12-26 12:30:54 +0800

    共用 函式庫

---
+++ Common_class.php
@@ -0,0 +1,61 @@
+<?php if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+/***
+ * 共用 函式庫
+ *
+ *
+ *
+ *
+ *
+ */
+class Common_class
+{
+
+    /**
+     * replace_symbol_text  將字串部分內容替換成星號或其他符號
+     *
+     * author：wesley
+     *
+     * editor : Jim
+     *
+     * @param string $len 長度
+     * @param string $string 原始字串
+     * @param string $symbol 替換的符號
+     * @param int $begin_num 顯示開頭幾個字元
+     * @param int $end_num 顯示結尾幾個字元
+     *
+     * @return string
+     */
+    public function replace_symbol_text($len, $string, $symbol, $begin_num = 0, $end_num = 0)
+    {
+
+        if ($len > 2) {
+            #多字模式
+            $string_length = mb_strlen($string);
+            $begin_num = (int) $begin_num;
+            $end_num = (int) $end_num;
+            $string_middle = '';
+            $symbol_num = $string_length - ($begin_num + $end_num);
+            $string_begin = mb_substr($string, 0, $begin_num);
+            $string_end = mb_substr($string, -1, $end_num);
+
+            for ($i = 0; $i < $symbol_num; $i++) {
+                $string_middle .= $symbol;
+            }
+
+            return $string_begin . $string_middle . $string_end;
+        }
+
+        if ($len == 2) {
+            #兩字模式
+
+            return str_replace(mb_substr($string, -1), "O", $string);
+        }
+
+    }
+
+}
+
+/* End of file Common_class.php */
