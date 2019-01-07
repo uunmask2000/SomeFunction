0801e44d9cc7f8f7dbdc033c4716bc8dd82637c1
Jim <uunmask2000@gmail.com>
2019-01-07 09:46:34 +0800

    一個Http 檔案下載的方法。

--- Common_class.php
+++ Common_class.php
@@ -200,6 +200,90 @@ class Common_class
         return preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $str);
     }
 
+    /**
+     * File_download 檔案下載
+     *
+     * @param  mixed $file 檔案路徑
+     * @param  mixed $file_name_N 檔案下載的名稱
+     *
+     * @return void
+     */
+    public function File_download($file, $file_name_N)
+    {
+
+        //First, see if the file exists
+        if (!is_file($file)) {die("<b>404 File not found!</b>");}
+
+        //Gather relevent info about file
+        $len = filesize($file);
+        $filename = basename($file);
+        $file_extension = strtolower(substr(strrchr($filename, "."), 1));
+
+        //This will set the Content-Type to the appropriate setting for the file
+        switch ($file_extension) {
+            case "pdf":$ctype = "application/pdf";
+                break;
+            case "exe":$ctype = "application/octet-stream";
+                break;
+            case "zip":$ctype = "application/zip";
+                break;
+            case "doc":$ctype = "application/msword";
+                break;
+            case "xls":$ctype = "application/vnd.ms-excel";
+                break;
+            case "ppt":$ctype = "application/vnd.ms-powerpoint";
+                break;
+            case "gif":$ctype = "image/gif";
+                break;
+            case "png":$ctype = "image/png";
+                break;
+            case "jpeg":
+            case "jpg":$ctype = "image/jpg";
+                break;
+            case "mp3":$ctype = "audio/mpeg";
+                break;
+            case "wav":$ctype = "audio/x-wav";
+                break;
+            case "mpeg":
+            case "mpg":
+            case "mpe":$ctype = "video/mpeg";
+                break;
+            case "mov":$ctype = "video/quicktime";
+                break;
+            case "avi":$ctype = "video/x-msvideo";
+                break;
+            case "txt":$ctype = "text/plain";
+                break;
+
+            //The following are for extensions that shouldn't be downloaded (sensitive stuff, like php files)
+            case "php":
+            case "htm":
+            case "html":
+                die("<b>Cannot be used for " . $file_extension . " files!</b>");
+                break;
+
+            default:$ctype = "application/force-download";
+        }
+
+        //Begin writing headers
+        header("Pragma: public");
+        header("Expires: 0");
+        header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+        header("Cache-Control: public");
+        header("Content-Description: File Transfer");
+
+        //Use the switch-generated Content-Type
+        header("Content-Type: $ctype");
+
+        //Force the download
+        $header = "Content-Disposition: attachment; filename=" . $file_name_N . ";";
+        header($header);
+        header("Content-Transfer-Encoding: binary");
+        header("Content-Length: " . $len);
+        @readfile($file);
+        exit;
+    }
+
 }
 
 /* End of file Common_class.php */
