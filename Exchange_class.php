9c1b416b54ba9c4f7183e2eab85bfbc89e64a957
Jim <uunmask2000@gmail.com>
2019-01-11 15:10:53 +0800

    修改兼具

--- Exchange_class.php
+++ Exchange_class.php
@@ -5,12 +5,14 @@
 class Exchange_class
 {
 
+    ## "validRanges":["1d","5d","1mo","3mo","6mo","1y","2y","max"]}
+    ##
     private $api_url;
     public function __construct($data = array())
     {
         if (!empty($data)) {
             ### $data['api_key'] 範例 :  TWDJPY
-            $this->api_url = 'https://partner-query.finance.yahoo.com/v8/finance/chart/' . $data['api_key'] . '=X?range=1d&corsDomain=tw.money.yahoo.com&interval=5m&includePrePost=false&.tsrc=yahoo-tw';
+            $this->api_url = 'https://partner-query.finance.yahoo.com/v8/finance/chart/' . $data['api_key'] . '=X?range=1y&corsDomain=tw.money.yahoo.com&interval=1mo&includePrePost=false&.tsrc=yahoo-tw';
         }
     }
 
@@ -40,17 +42,22 @@ class Exchange_class
      */
     public function sub($response)
     {
+        // print_r($response);
         $new_array = array();
         $new_array = array(
             "title" => $response['chart']['result'][0]['meta']['symbol'],
-            "chartPreviousClose" => $response['chart']['result'][0]['meta']['chartPreviousClose'],
-            "previousClose" => $response['chart']['result'][0]['meta']['previousClose'],
             "timestamp" => $response['chart']['result'][0]['timestamp'],
+            "high" => $response['chart']['result'][0]['indicators']['quote'][0]['high'],
             "low" => $response['chart']['result'][0]['indicators']['quote'][0]['low'],
             "close" => $response['chart']['result'][0]['indicators']['quote'][0]['close'],
             "open" => $response['chart']['result'][0]['indicators']['quote'][0]['open'],
             "volume" => $response['chart']['result'][0]['indicators']['quote'][0]['volume'],
         );
+
+        ## 新增轉化時間格式
+        foreach ($new_array['timestamp'] as $key => $value) {
+            $new_array['timestamp_date'][$key] = date("Y-m-d H:i:s", $value);
+        }
 
         return $new_array;
     }
