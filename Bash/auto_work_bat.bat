7f1d9a3e4f56cd3857341e8885098d95a3b95f09
uunmask2000 <uunmask2000@gmail.com>
2019-02-12 09:24:45 +0800

    新增修改 判断是否见建立，没有建立产生一个标题文件

--- Bash/auto_work_bat.bat
+++ Bash/auto_work_bat.bat
@@ -1,5 +1,4 @@
 @echo off
-
 SET TodayYear=%date:~0,4%
 SET TodayMonthP0=%date:~5,2%
 SET TodayDayP0=%date:~8,2%
@@ -18,17 +17,16 @@ IF %TodayMonthP0:~0,1% == 0 (
 
 echo da1 %TodayYear%-%TodayMonth%-%TodayDay%
 echo da2 %TodayYear%-%TodayMonthP0%-%TodayDayP0%  
- 
+
 pause
 
-
-file=  %TodayYear%-%TodayMonthP0%-%TodayDayP0%.txt 
-if [ -f "$file" ]
-then
-	echo "$file found."
-else
-	copy /y nul %TodayYear%-%TodayMonthP0%-%TodayDayP0%.txt 
-fi
-pause
-
-
+@echo off
+REM 声明采用UTF-8编码
+chcp 65001  
+if exist %TodayYear%-%TodayMonthP0%-%TodayDayP0%.txt (
+	echo "檔案已存在"
+) else ( 
+	copy /y nul %TodayYear%-%TodayMonthP0%-%TodayDayP0%.txt
+	@echo  【%TodayMonthP0%-%TodayDayP0%工作日报】 >> %TodayYear%-%TodayMonthP0%-%TodayDayP0%.txt 
+)
+pause 
